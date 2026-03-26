841e4f3ac2c7a3277254083b27990a3bd11ac236
ramyakumar-2 <ramyakumar0401@gmail.com>
2026-03-26 14:33:46 +0530

    gh

---
+++ random.py
@@ -0,0 +1,21 @@
+print("ramya")
+
+
+# cd Desktop/dice-project
+# git init
+# git add .
+# git commit -m "Initial commit - Dice rolling project"
+# git remote add origin https://github.com/ramyakumar-2/ML.git
+# git branch -M main
+# git push -u origin main
+
+
+
+
+"cd Desktop/dice-project
+git init
+git add .
+git commit -m "Initial commit - Dice rolling project"
+git remote add origin https://github.com/your-username/dice-project.git
+git branch -M main
+git push -u origin main"
