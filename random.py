60b2d92fe601be0a0feefac60bdff678bdd3616e
ramyakumar-2 <ramyakumar0401@gmail.com>
2026-03-26 14:12:02 +0530

    my_first_commit

--- random.py
+++ random.py
@@ -1,18 +1,1 @@
-import random
-
-def roll_dice():
-    return random.randint(1, 6)
-
-while True:
-    user = input("Roll the dice? (yes/no): ").lower()
-    
-    if user == "yes":
-        dice = roll_dice()
-        print("🎲 You got:", dice)
-        
-    elif user == "no":
-        print("Thanks for playing!")
-        break
-    
-    else:
-        print("Invalid input! Please type yes or no.")
+print("Ramya Kumar")
